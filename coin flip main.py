0f5c04b8ab1035ee193bf506fdb9ca0364945ec2
newtype89-dev <newtype89.art@gmail.com>
2025-06-03 18:04:34 -0400

    FInal code
    this one finally works as intended

--- coin flip main.py
+++ coin flip main.py
@@ -1,23 +1,14 @@
-import random
+import secrets
 
 def flip():
-    result= random.randint(0,1)
-    if result ==0:
-        result == "heads"
-    else:
-        result == "tails"
-        
-print("lets play a game Heads or tails?")
-flip_result= flip 
-guess_heads= input("head")
-guess_tails= input("tailes")
+    return secrets.choice(["heads", "tails"])
 
-if guess_heads == flip_result:
-    print("congrats thats right")
-else:
-    print("sorry try again")
 
-if guess_tails == flip_result:
-       print("congrats thats right")
-else:
-    print("sorry try again")
+
+print("lets play a game guess hears or tails")
+
+guess= input("whats your guess (heads/tails):").strip().lower()
+
+result=flip()
+
+print("yo good job yout right!! play again?" if guess== result else "aahh sorry not this time. try again?")
